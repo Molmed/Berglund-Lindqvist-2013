c27692773eb9f9b2bdfeb92dcd824479bb22ee8b
Carl Mårten Lindqvist <bokelund@gmail.com>
2013-09-23 10:48:04 +0200

    clean 	modified:   BasicFixedPointSNVcaller.py

--- BasicFixedPointSNVcaller.py
+++ BasicFixedPointSNVcaller.py
@@ -19,15 +19,7 @@ import sys
 import re
 from collections import defaultdict
 
-
-# Setup
-#bamFile="<input bam file>"
-#reference="<referece>"
-#vcf = "<vcf reference>"
-#PhredCutoff = only bases above this will be considered for allele-freq-calculation (AFC)
-#MapqCutoff = only reads with a mapq over this will be considered when calculating raw depth and AFC
-
-#using arguments (in a lazy way) for this
+#using arguments
 bamFile = sys.argv[1]
 reference = sys.argv[2]
 vcf = sys.argv[3]
@@ -36,12 +28,8 @@ PhredCutoff = sys.argv[4]
 PhredCutoff = int(PhredCutoff)
 MapqCutoff = sys.argv[5]
 MapqCutoff = int(MapqCutoff)
-#special for extracting name from filename, in pilot
-#NameOfSample = regex.findall(bamFile)[0]
-#NameOfSample = "RNAseqSample"
-NameOfSample = "Sample"
 
-#print str(PhredCutoff) + " " + str(MapqCutoff)
+NameOfSample = "Sample"
 
 MaxDepthCutoff = 1000000
 
@@ -53,7 +41,7 @@ baseCountGlobal = {'A': 0,'C': 0,'G': 0,'T': 0,'.': 0, 'N': 0 , 'RawDepth' : 0}
 def getBase(chrom,pos,bamFile):
      baseCount = {'A': 0,'C': 0,'G': 0,'T': 0,'CoverDel': 0, 'N': 0, 'RawDepth' : 0, 'FlankIns' : 0 ,'FlankDel' : 0}
      startOfRead = defaultdict(int)
-     #for pileupColumn in bamFile.pileup(chrom, pos - 2, pos + 2):
+
      for pileupColumn in bamFile.pileup(chrom, pos - 2, pos + 2,max_depth=MaxDepthCutoff):
           if(pileupColumn.pos == pos - 1):
                for pileupRead in pileupColumn.pileups:
@@ -73,11 +61,8 @@ def getBase(chrom,pos,bamFile):
                     #add check that base is ACGT ...
                     elif phreadQual > PhredCutoff and pileupRead.alignment.mapq > MapqCutoff and pileupRead.alignment.seq[pileupRead.qpos] in Bases:
                          baseCount[pileupRead.alignment.seq[pileupRead.qpos]] += 1
-                         #print str(pileupRead.alignment.seq[pileupRead.qpos])
-                    #print str(phreadQual) + " " + str(pileupRead.alignment.mapq)
-                    #print pileupRead.alignment.seq[pileupRead.qpos]
-                    #if pileupRead.is_del:
-                         #print "deletion with phred" + "\t" + str(phreadQual)
+                    
+                    
                          
                     
      return baseCount
@@ -87,10 +72,7 @@ samFile = pysam.Samfile( bamFile, "rb" )
 fastaFile = pysam.Fastafile(reference)
 vcfFile = open(vcf,'r')
 
-# Actual parsing and writing
-#print "\tpos" + "\t" + "ref" + "\t" "alt" + "\t" + "hapmap" + "\t" + "A" + "\t" + "C" + "\t" + "G" + "\t" + "T" + "\t" + "." + "\t" + "N"
-#changing so columns follows column-names
-#print "\tpos" + "\t" + "ref" + "\t" "alt" + "\t" + "hapmap" + "\t" + "\t".join(str(x) for x in baseCountGlobal.keys())
+
 
 print "##fileformat=VCFv4.1"
 print "##this is called with BasicFixedPointSNVcaller.py"
@@ -103,13 +85,8 @@ for line in vcfFile:
           pos  = line.split('\t')[1]
           pos=int(pos)
           #using ref from fasta and calculates alt from baseCounts
-          #ref =  line.split('\t')[3]
-          #alt =  line.split('\t')[4]
-          #alt = re.sub("\,.*","",alt)
           if "INDEL" in line:
                continue
-
-          #genotypeInRef =  line.split('\t')[10].strip("\n")
           refFromFasta = fastaFile.fetch(chrome,pos-1,pos)
           baseCounts = getBase(chrome,pos,samFile)
           if baseCounts == None:
@@ -126,18 +103,10 @@ for line in vcfFile:
                          altBase = i
 
 
-          #Print-out for test               
-          #print str(chrome) + "\t" + str(pos)  + "\t" +  str(baseCounts[refFromFasta]) + "\t" + refFromFasta + "\t" + str(altCount)  + "\t" +  altBase + "\t" + str(baseCounts['CoverDel'])  + "\t" + "Cover Deletion"  + "\t" + str(baseCounts['RawDepth']) + "\t" +  "RawDepth" + "\t" +  str(baseCounts['FlankIns']) + "\t" + "Flanking insertion" + "\t" +  str(baseCounts['FlankDel']) + "\t" + "Flanking deletion"
-          
           allele_freq = 0
           if baseCounts[refFromFasta] + altCount > 0:
                allele_freq = altCount/(baseCounts[refFromFasta] + altCount)
           print str(chrome) + "\t" + str(pos) + "\t" + "."  + "\t" +  refFromFasta + "\t" + altBase + "\t" + "." + "\t" + "PASS" + "\t" + "." + "\t" + "DP4;Flankinsert,FlankDel,CoverDel"  + "\t" + str(baseCounts['A']) + "," + str(baseCounts['C']) + ","+ str(baseCounts['G']) + ","+ str(baseCounts['T'])  + ";" + str(baseCounts['FlankIns']) + "," + str(baseCounts['FlankDel']) + "," + str(baseCounts['CoverDel'])  + "\t" + str(baseCounts['RawDepth'])  + "\t" +  str(baseCounts[refFromFasta]) + "\t" +  str(altCount)  + "\t" + str(allele_freq)
-
-#allele freq does not work if both are zero
-#str(float(altCount)/float(altCount + baseCounts[refFromFasta]))
-
-#"\trawdepth\tref_reads\talt_reads\tallele_freq\n"
 
 samFile.close()
 fastaFile.close()
